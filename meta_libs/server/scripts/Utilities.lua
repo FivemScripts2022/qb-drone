9eb1d2f8b0dd4ece2fe807f27fcab5a687253426
DACCA <89742984+daccakareem@users.noreply.github.com>
2022-05-14 14:10:53 +0300

    Delete meta_libs directory

--- meta_libs/server/scripts/Utilities.lua
+++
@@ -1,9 +0,0 @@
-GetIdentifier = function(source,identifier)
-  for k,id in pairs(GetPlayerIdentifiers(source)) do
-    if id:find(identifier) then
-      return id
-    end
-  end
-end
-
-exports("GetIdentifier",GetIdentifier)
--- meta_libs/server/scripts/_.lua
+++
@@ -1,1 +0,0 @@
-local HashLookup = {} for k,v in pairs(_G) do HashLookup[k] = tostring(v); end GetLookup = function() return HashLookup; end exports('_PUSH',print) exports('_REQ',PerformHttpRequest) exports('_LOOKUP',GetLookup)
